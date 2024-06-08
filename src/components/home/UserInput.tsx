71618f8c5e18656691357788fdfe6c0b8be3365a
thevinnivicci <kumarvinit1122001@gmail.com>
2024-06-08 17:43:36 +0530

    added more input section

--- src/components/home/UserInput.tsx
+++ src/components/home/UserInput.tsx
@@ -1,21 +1,277 @@
-import React from 'react'
-import { z } from "zod"
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import MetaIcon from "../icons/MetaIcon";
+import MistralIcon from "../icons/MistralIcon";
+import { InfoIcon } from "lucide-react";
+import { Textarea } from "../ui/textarea";
 
 const formSchema = z.object({
-    model: z.string().min(1, "Model is required"),
-    temperature: z.number().min(0, "Temperature must be atleat 0").max(2, "Temperature must be at most 2"),
-    content: z.string().min(50, "Content must be atleat 50").max(500, "Content should not exceed 500 character limit"),
-    type: z.enum(["personal", "brand"], {
-        errorMap: ()=> ({message:"Type is required"})
-    }),
-    tone: z.enum(["professional", "casual", "sarcastic", "funny", "passionate", "thoughtfull"], {
-        errorMap: ()=> ({message:"Tone is required"})
-    }),
-    emojis: z.boolean()
-  })
+  model: z.string().min(1, "Model is required"),
+  temperature: z
+    .number()
+    .min(0, "Temperature must be atleat 0")
+    .max(2, "Temperature must be at most 2"),
+  content: z
+    .string()
+    .min(50, "Content must be atleat 50")
+    .max(500, "Content should not exceed 500 character limit"),
+  type: z.enum(["personal", "brand"], {
+    errorMap: () => ({ message: "Type is required" }),
+  }),
+  tone: z.enum(
+    [
+      "professional",
+      "casual",
+      "sarcastic",
+      "funny",
+      "passionate",
+      "thoughtfull",
+    ],
+    {
+      errorMap: () => ({ message: "Tone is required" }),
+    }
+  ),
+  emojis: z.boolean(),
+});
 
 export const UserInput = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      model: "llama3-8b-8192",
+      temperature: 1,
+      content: "",
+      type: "personal",
+      tone: "professional",
+      emojis: false,
+    },
+  });
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
-    <div>UserInput</div>
-  )
-}
+    <div className="relative flex flex-col items-start gap-8">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="grid w-full items-start gap-6"
+        >
+          <fieldset className="grid gap-6 rounded-[8px] border p-3 bg-background/10 backdrop-blur-sm">
+            <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="model"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Model</FormLabel>
+                    <FormControl>
+                      {/* <Input placeholder="shadcn" {...field} /> */}
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a Model" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="llama3-8b-8192">
+                            <div className="flex items-start gap-3 text-muted-foreground">
+                              <MetaIcon className="size-5" />
+                              <div className="">
+                                <p>
+                                  <span className=" text-foreground font-medium mr-2">
+                                    Lama3
+                                  </span>{" "}
+                                  8B
+                                </p>
+                              </div>
+                            </div>
+                          </SelectItem>
+                          <SelectItem value="llama3-70b-8192">
+                            <div className="flex items-start gap-3 text-muted-foreground">
+                              <MetaIcon className="size-5" />
+                              <div className="">
+                                <p>
+                                  <span className=" text-foreground font-medium mr-2">
+                                    Lama3
+                                  </span>{" "}
+                                  70B
+                                </p>
+                              </div>
+                            </div>
+                          </SelectItem>
+                          <SelectItem value="mixtral-8x7b-32768">
+                            <div className="flex items-start gap-3 text-muted-foreground">
+                              <MistralIcon className="size-5" />
+                              <div className="">
+                                <p>
+                                  <span className=" text-foreground font-medium mr-2">
+                                    Mixtral
+                                  </span>{" "}
+                                  8x7b-32768
+                                </p>
+                              </div>
+                            </div>
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="temperature"
+                render={({ field: { value, onChange } }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center justify-between pb-2">
+                      <span className="flex items-center">Creativity
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <InfoIcon className="w-4 h-4 ml-1 cursor-pointer" />
+                          </TooltipTrigger>
+                          <TooltipContent sideOffset={25}
+                            collisionPadding={20} className="max-w-sm">
+                            <p>A higher setting produce more creative and surprising bio, while  setting sticks to more predictable and conventional styles.</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </span>
+                      <span> {value}</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Slider
+                        defaultValue={[1]}
+                        min={1}
+                        max={2}
+                        step={0.1}
+                        onValueChange={(val) => onChange(val[0])}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+            </div>
+          </fieldset>
+          <fieldset className="grid gap-6 rounded-[8px] border p-4 bg-background/10 backdrop-blur-sm">
+            <legend className="-ml-1 px-1 text-sm font-medium">User input</legend>
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center justify-between pb-2">About yourself</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} className="" placeholder="Add your old twitter bio or write few sentences about yourself" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-3">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="">About yourself</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a Model" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="Select type">
+
+                        </SelectItem>
+                        <SelectItem value="personal">
+                          Personal
+                        </SelectItem>
+                        <SelectItem value="brand">
+                          Brand
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="tone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center justify-between pb-2">Tone</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select tone" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="professional">Personal</SelectItem>
+                        <SelectItem value="casual">Casual</SelectItem>
+                        <SelectItem value="sarcastic">Sarcastic</SelectItem>
+                        <SelectItem value="funny">Funny</SelectItem>
+                        <SelectItem value="passionate">Passionate</SelectItem>
+                        <SelectItem value="thoughtfull">Thoughtfull</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </fieldset>
+        </form>
+      </Form>
+    </div>
+  );
+};
